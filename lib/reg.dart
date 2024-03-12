48a9e02f02537dc1abdd15a9d5b83c41a7c31348
diya <diyashehenkn@gmail.com>
2024-03-12 17:02:56 +0530

    reg

---
+++ lib/reg.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('login',style: TextStyle(
+        fontSize: 30,color:Colors.red,fontWeight: FontWeight.bold),
+    ),
+      ),
+    );
+  }
+}
